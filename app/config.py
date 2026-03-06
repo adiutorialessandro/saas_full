8418906e2ffeeafef405923372cc122e5d642e76
Alessandro Adiutori <alessandroadiutori@MAC-3D7C6A.station>
2026-03-07 00:20:37 +0100

    Use Postgres on Render and fix RegisterForm

--- app/config.py
+++ app/config.py
@@ -1,10 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "dev-secret-change-me")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///dev.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or os.environ.get("SECRET_KEY") or "dev-secret"
 
-    # CSRF/WTF
-    WTF_CSRF_ENABLED = True
-    WTF_CSRF_TIME_LIMIT = None
+    db_url = os.environ.get("DATABASE_URL", "sqlite:///dev.db")
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, FloatField, RadioField
-from wtforms.validators import DataRequired, Email, Length, NumberRange, Optional, Regexp
+from wtforms.validators import DataRequired, Email, Length, NumberRange, Optional, Regexp, EqualTo
 
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    password2 = PasswordField(
+        "Ripeti Password",
+        validators=[DataRequired(), EqualTo("password", message="Le password non coincidono")]
+    )
     submit = SubmitField("Registrati")
 
 
@@ -35,8 +39,8 @@ class EssentialDataForm(FlaskForm):
     clienti_mese = FloatField("Clienti mese (#)", validators=[Optional(), NumberRange(min=0)])
     submit = SubmitField("Continua")
 
+
 class QuizForm(FlaskForm):
-    # 1 = alto rischio, 5 = basso rischio
     _choices = [(1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")]
 
     q1 = RadioField("Q1", choices=_choices, coerce=int, validators=[DataRequired()])
